e83393d4fe005de72ee6e021eeb1f577b75e42b3
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-21 15:19:28 +0100

    finish

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
-let player = $('.player__video viewer').first();
+let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
 
-$('.player__button toggle').click(function () {
+$("button").click(function (event) {
+    if($(this).is(".toggle")) {
+        player.trigger("play");
+        $(this).removeClass("toggle");
+        $(this).text("⏸")
+    }
+    else {
+        player.trigger("pause");
+        $(this).text("►");
+        $(this).addClass("toggle");
+    }
+})
 
+player.on("timeupdate",function () {
+    progress.css ( {
+        "width": 100 * this.currentTime/this.duration
+    })
 })
